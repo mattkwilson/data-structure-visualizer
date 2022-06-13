6d435fb7648da7c351fdd1eb5ce94799e8fd408d
Tarek Ayoubi <tarek20501@hotmail.com>
2022-06-12 17:50:42 -0700

    Implemented and tested instrument method in instrumentor class

--- DataStructureVisualizer/src/Instrumentor.java
+++ DataStructureVisualizer/src/Instrumentor.java
@@ -1,5 +1,12 @@
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import exceptions.InvalidClassNameException;
+import exceptions.InvalidFieldName;
 
+import java.util.LinkedList;
 import java.util.List;
 
 // Add the analyzer class to the project under analysis
@@ -24,10 +31,12 @@ public class Instrumentor {
     }
 
     private void instrumentPublicField() {
+        System.out.println("Instrumenting public field...");
         // TODO: Matt
     }
 
     private void instrumentPrivateField() {
+        System.out.println("Instrumenting private field...");
         // TODO: Tarek
     }
 
@@ -37,14 +46,62 @@ public class Instrumentor {
      * Determine which case and call the proper function
      * */
     public void instrument() {
-        // TODO: Tarek
+        System.out.println("Stared instrumenting the code...");
+        // Find the class
+        List<ClassOrInterfaceDeclaration> classes = getNodes(compilationUnits, ClassOrInterfaceDeclaration.class);
+        ClassOrInterfaceDeclaration classUnderAnalysis = null;
+        for (ClassOrInterfaceDeclaration c : classes) {
+            if (c.getName().toString().equals(this.className)) {
+                classUnderAnalysis = c;
+            }
+        }
+        if (classUnderAnalysis == null) {
+            throw new InvalidClassNameException();
+        }
+        // Find the member
+        List<VariableDeclarator> variables = classUnderAnalysis.findAll(VariableDeclarator.class);
+        variables = variables.stream().filter(v -> v.getName().toString().equals(fieldName)).toList();
+        if (variables.size() != 1) {
+            throw new InvalidFieldName();
+        }
+        VariableDeclarator variable = variables.get(0);
+        if (!(variable.getParentNode().get() instanceof FieldDeclaration)) {
+            throw new InvalidFieldName();
+        }
+        FieldDeclaration field = (FieldDeclaration) variable.getParentNode().get();
+        // Call the right function based on modifier
+        List<Modifier.Keyword> modifiers = field.getModifiers().stream().map(Modifier::getKeyword).toList();
+        switch (modifiers.size()) {
+            case 2:
+                if (!modifiers.contains(Modifier.Keyword.FINAL)) {
+                    throw new InvalidFieldName();
+                }
+            case 1:
+                if (modifiers.contains(Modifier.Keyword.PUBLIC)) {
+                    instrumentPublicField();
+                } else if (modifiers.contains(Modifier.Keyword.PRIVATE)) {
+                    instrumentPrivateField();
+                } else {
+                    throw new InvalidFieldName();
+                }
+                break;
+            default:
+                throw new InvalidFieldName();
+        }
+        System.out.println("Finished instrumenting the code!");
+    }
+
+    private static List getNodes(List<CompilationUnit> ast, Class nodeClass) {
+        List res = new LinkedList<>();
+        ast.forEach(cu -> res.addAll(cu.findAll(nodeClass)));
+        return res;
     }
 
     /**
-     *  Run the instrumented code
+     *  Add the analyzer class to the project under analysis
+     *  Run the instrumented code or save it to disk, so it can be run manually
      * */
     public void runDynamicAnalysis() {
         // TODO: TBD
     }
-
 }
