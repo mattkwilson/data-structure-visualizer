8d79c53722c9f862a6b9bf786410efa8f2580c45
mertkipcak <mkipcak@students.cs.ubc.ca>
2022-06-12 18:54:55 -0700

    Implemented the analyzer and the JSON saving feature

--- DataStructureVisualizer/src/Analyzer.java
+++ DataStructureVisualizer/src/Analyzer.java
@@ -1,38 +1,60 @@
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Analyzer {
 
-    private ArrayList states;
+    private final JSONArray states;
+    private final String name;
+    private final int instanceNumber;
 
-    public Analyzer() {
-        states = new ArrayList<>();
+    public Analyzer(String name, int instanceNumber) {
+        this.states = new JSONArray();
+        this.name = name;
+        this.instanceNumber = instanceNumber;
     }
 
-    public <T> void analyze(Collection<T> collection, String name) {
+    public <T> void analyze(Collection<T> collection) {
         StackTraceElement stack = new Throwable().getStackTrace()[1];
         String fileName = stack.getFileName();
         int lineNumber = stack.getLineNumber();
-        String content = collection.toString();
-        String type = collection.getClass().getName();
+        String contents = collection.toString();
+        String structType = collection.getClass().getName();
+        addState(fileName, lineNumber, structType, contents);
     }
 
-    public static <K, V> void analyze(HashMap<K, V> map, String name) {
+    public <K, V> void analyze(HashMap<K, V> map) {
         StackTraceElement stack = new Throwable().getStackTrace()[1];
         String fileName = stack.getFileName();
         int lineNumber = stack.getLineNumber();
-        String content = map.toString();
-        String type = map.getClass().toString();
+        String contents = map.toString();
+        String structType = map.getClass().toString();
+        addState(fileName, lineNumber, structType, contents);
     }
 
-    private void addStateToJSON() {
-
+    private void addState(String fileName, int lineNumber, String structType, String contents) {
+        JSONObject state = new JSONObject();
+        state.put("fileName", fileName);
+        state.put("lineNumber", lineNumber);
+        state.put("name", name);
+        state.put("structType", structType);
+        state.put("contents", contents);
+        states.put(state);
     }
 
-    private void writeJson() {
-
+    private void writeJSON() {
+        try {
+            FileWriter writer = new FileWriter(name + "_" + instanceNumber + ".json");
+            writer.write(states.toString());
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
