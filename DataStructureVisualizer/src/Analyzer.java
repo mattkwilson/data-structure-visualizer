a2a0a21da53003dc9fbd1224d23c5b98130c3d03
mertkipcak <mkipcak@students.cs.ubc.ca>
2022-06-13 23:23:01 -0700

    Implemented the static Analyzer

--- DataStructureVisualizer/src/Analyzer.java
+++ DataStructureVisualizer/src/Analyzer.java
@@ -1,64 +1,78 @@
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+@SuppressWarnings("unused")
 public class Analyzer {
 
-    private final JSONArray states;
-    private final String name;
-    private final int instanceNumber;
+    private class Structure {
+        public String name;
+        public JSONArray states;
+        public final int instanceNumber;
 
-    private Map<Object, JSONArray> instanceMap;
+        public Structure(String name, int instanceNumber) {
+            this.name = name;
+            this.states = new JSONArray();
+            this.instanceNumber = instanceNumber;
+        }
+    }
 
-    public Analyzer(String name, int instanceNumber) {
-        this.states = new JSONArray();
-        this.name = name;
-        this.instanceNumber = instanceNumber;
+    private static int nextInstanceNumber = 0;
+    private final Map<Integer, Structure> instanceMap;
+
+    public Analyzer() {
+        instanceMap = new HashMap<>();
     }
 
     public void createInstance(Object instance, String name) {
-        // instanceMap.put(instance, new JSONArray());
-        // add instance to map
-        // TODO: implement
+        instanceMap.put(System.identityHashCode(instance), new Structure(name, nextInstanceNumber));
+        System.out.println(System.identityHashCode(instance));
+        nextInstanceNumber++;
     }
 
     public void setInstance(Object instance, String name) {
 
     }
 
-    public void analyze(Object object) {
+    public void analyze(Object object) throws RuntimeException {
         StackTraceElement stack = new Throwable().getStackTrace()[1];
+        // Check if the object is one of the ones getting tracked
+        if (!instanceMap.containsKey(System.identityHashCode(object))) {
+            return;
+        }
+
+        // Fetch the required data for json
+        Structure structure = instanceMap.get(System.identityHashCode(object));
         String fileName = stack.getFileName();
         int lineNumber = stack.getLineNumber();
         String contents = object.toString();
         String structType = object.getClass().toString();
 
-        addState(fileName, lineNumber, structType, contents);
-    }
-
-    private void addState(String fileName, int lineNumber, String structType, String contents) {
+        // Create the JSON object called state and assign variables
         JSONObject state = new JSONObject();
         state.put("fileName", fileName);
         state.put("lineNumber", lineNumber);
-        state.put("name", name);
+        state.put("name", structure.name);
         state.put("structType", structType);
         state.put("contents", contents);
-        states.put(state);
+
+        // Add the recent state to the states
+        structure.states.put(state);
     }
 
     public void writeJSON() {
         try {
-            FileWriter writer = new FileWriter(name + "_" + instanceNumber + ".json");
-            writer.write(states.toString());
-            writer.flush();
-            writer.close();
-            System.out.println("saved JSON at: " + name + "_" + instanceNumber + ".json");
+            for (Structure struct: instanceMap.values()) {
+                FileWriter writer = new FileWriter(struct.name + "_" + struct.instanceNumber + ".json");
+                writer.write(struct.states.toString());
+                writer.flush();
+                writer.close();
+                System.out.println("saved JSON at: " + struct.name + "_" + struct.instanceNumber + ".json");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
