247c7427f4f3828931536a95bd53c4f584e88eee
edvin <edvinkeskin@gmail.com>
2022-06-14 20:41:09 -0700

    added blue line to imply array element removal

--- frontend/src/components/Visual/ArrayList.jsx
+++ frontend/src/components/Visual/ArrayList.jsx
@@ -6,7 +6,7 @@ export default function ArrayList(props) {
             <tr>
                 {props.contents.map((element, i) => {
                     return(
-                        <td className = {`td${props.bluePosition === i ? '-blue' :
+                        <td key={i} className = {`td${props.bluePosition === i ? '-blue' :
                                               props.redPositions.includes(i) ? '-red' : ''}`}> {element}</td>
                     )
                 })}
