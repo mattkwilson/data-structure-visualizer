af079929133694cd98eac197d4160fa4961b1852
edvin <edvinkeskin@gmail.com>
2022-06-16 19:33:22 -0700

    hashmap visuals updates: fixed box size, resizing arrow

--- frontend/src/components/Visual/HashMap.jsx
+++ frontend/src/components/Visual/HashMap.jsx
@@ -1,12 +1,24 @@
 import './Visual.css';
 import arrow from '../../images/arrow.png';
+import {useEffect, useState} from "react";
 
 export default function HashMap(props) {
     var array1 = props.contents[0]
     var array2 = props.contents[1]
+    const [imageHeight, setImageHeight] = useState(0)
+
+    function setHeight() {
+        const height = document.getElementById('tableDiv').clientHeight
+        setImageHeight(height)
+    }
+
+    useEffect(() => {
+        setHeight()
+    })
+
     return (
         <div className="HashMap">
-            <table>
+            <table id='tableDiv'>
                 {array1.map((element, i) => {
                     return(
                         <tr key={i}>
@@ -16,7 +28,7 @@ export default function HashMap(props) {
                     )
                 })}
             </table>
-            <img src={arrow} alt="->" />
+            <img src={arrow} alt="->" id='img' height={imageHeight}/>
             <table>
                 {array2.map((element, i) => {
                     return(
